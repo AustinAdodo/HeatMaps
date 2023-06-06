8b8066a2d3be0ec40ae311cfce9f4d4b463f11b3
Odia Adodo <subzbelow@gmail.com>
2023-06-06 14:11:11 +0100

    Add project files.

---
+++ Controllers/SalesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HeatMaps.Controllers
+{
+    public class SalesController : Controller
+    {
+        public readonly ILogger _logger;
+        public SalesController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Check If API is up
+        public IActionResult Index()
+        {
+            return StatusCode(200);
+        }
+
+        //return all sales
+        [HttpGet]
+        public IActionResult GetAllSales()
+        {
+            return StatusCode(200);
+        }
+
+        [HttpGet]
+        public IActionResult GetSpecificProductSale(string Saleid)
+        {
+            return StatusCode(200);
+        }
+
+        [HttpGet]
+        public IActionResult GetSpecificProductSale(DateTime date)
+        {
+            return StatusCode(200);
+        }
+
+        //Add Sale
+        [HttpPost]
+        public IActionResult AddSale([FromBody] List<Product> Products, CancellationToken token)
+        {
+            Guid SaleId = Guid.NewGuid();
+            Dictionary<Guid,List < Product>> Result = new Dictionary<Guid,List<Product>>();
+            Result[SaleId] = Products;  
+            return Ok(Result);
+        }
+
+        //Delete Sale
+        [HttpPost]
+        public IActionResult DeleteSale(Guid SaleId, CancellationToken token)
+        {
+            return Ok(200);
+        }
+
+        //Edit Sale
+
+        [HttpPut]
+        public IActionResult EditSale(Guid SaleId, CancellationToken token)
+        {
+            return Ok(200);
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Product
+{
+    public int id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; } 
+    public DateTime CreatedDate { get; set;}
+    public DateTime UpdatedDate { get; set;}
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string ProductCategorty { get; set; }
+}
+
